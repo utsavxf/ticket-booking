3b9d3d8ba7bc6aa03ccbc12be06188ee9e8293de
utsavxf <utb4578@gmail.com>
2025-03-24 23:31:35 +0530

    object mapping,hashing password

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -3,12 +3,21 @@
  */
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class App {
     public String getGreeting() {
         return "Hello World!";
     }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+
+        List<Integer> l= Arrays.asList(1,2,3,4,5,6,7,8,9);
+        List<Integer> l1=l.stream().filter(i -> i%2==0).collect(Collectors.toList());
+        for(Integer i:l1){
+            System.out.println(i);
+        }
     }
 }
--- app/src/main/java/org/example/service/UserBookingService.java
+++ app/src/main/java/org/example/service/UserBookingService.java
@@ -1,6 +1,15 @@
 package org.example.service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.entites.User;
+import org.example.util.UserServiceUtil;
+
+import javax.swing.text.html.Option;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
 
 public class UserBookingService {
 
@@ -8,8 +17,45 @@ public class UserBookingService {
     //that's why we are storing it here
     private User user;
 
-    public UserBookingService(User user1){
-      this.user=user1;
+    private List<User> userList;
+
+    private static final String USERS_PATH = "app/src/main/java/org.example/localDb/users.json";
+
+
+    //now in our model and in the local db files that we have made one is userId in camel case and user_id in snake case
+    //so to map this we need to have an object mapping and we need to use a library for this also
+    //and aslo object se json me daalne ke liye serialize karna padega and vice-versa de-serialize karna padega
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+
+    public UserBookingService(User user1) throws IOException {
+        this.user = user1;
+        File users = new File(USERS_PATH); //I have plain text in this users
+        userList = objectMapper.readValue(users, new TypeReference<List<User>>() {
+        }); //List expects a generic type E, you can check so to provide a specific type (User) at runtime we need to do this})
+
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser=userList.stream().filter(user1 -> {
+            return user1.getName().equalsIgnoreCase( user.getName()) && UserServiceUtil.checkPassword(user.getPassword(),user1.getHashedPassword());
+        }).findFirst(); //let's say humari list me 2 bande hai jinke same name aur password hai to jo pehele number be padha hua hai vo use dedega
+          return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try {
+            userList.add(user1);
+            saveUserListToFile();
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException{
+        File usersFile=new File(USERS_PATH);
+        objectMapper.writeValue(usersFile,userList); //here serialization is taking place
     }
 
 }
